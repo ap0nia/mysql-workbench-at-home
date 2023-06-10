276cb3d6240f815e1a66327cb23933c975814c60
senti <brianmv@uci.edu>
2023-06-10 07:57:14 -0700

    added header content

--- apps/mysql-workbench/src/lib/components/Header.svelte
+++ apps/mysql-workbench/src/lib/components/Header.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-  import { AppBar } from '@skeletonlabs/skeleton'
+  import { AppBar, LightSwitch } from '@skeletonlabs/skeleton'
 </script>
 
-<AppBar>MySQL</AppBar>
+<AppBar>
+  <svelte:fragment slot="lead">
+    <div>
+      <img src="/mysql-logo.svg" alt="MySQL Logo" width="48" height="48" />
+    </div>
+  </svelte:fragment>
+  <div>
+    <h1 class="text-center h1">MySQL Workbench</h1>
+  </div>
+  <svelte:fragment slot="trail">
+    <LightSwitch />
+  </svelte:fragment>
+</AppBar>
