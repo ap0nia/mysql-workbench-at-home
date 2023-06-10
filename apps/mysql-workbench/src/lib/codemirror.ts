4d2513040a58b4e6dc21cb9d0749e870c57a98b1
senti <brianmv@uci.edu>
2023-06-10 15:06:56 -0700

    ready for homework

--- apps/mysql-workbench/src/lib/codemirror.ts
+++ apps/mysql-workbench/src/lib/codemirror.ts
@@ -33,7 +33,7 @@ import {
 
 export const value = writable('')
 
-export const extensions: Extension = [
+export const extensions = [
   json(),
   lineNumbers(),
   highlightActiveLineGutter(),
@@ -66,34 +66,38 @@ export const extensions: Extension = [
     ...completionKeymap,
     ...lintKeymap,
   ]),
-]
+] satisfies Extension
 
-export const state = EditorState.create({
-  doc: JSON.stringify(
-    {
-      query_block: {
-        select_id: 1,
-        cost_info: {
-          query_cost: '72.90',
-        },
-        table: {
-          table_name: 'Item',
-          access_type: 'ALL',
-          rows_examined_per_scan: 719,
-          rows_produced_per_join: 719,
-          filtered: '100.00',
+export function createState(extensions: Extension = []) {
+  return EditorState.create({
+    doc: JSON.stringify(
+      {
+        query_block: {
+          select_id: 1,
           cost_info: {
-            read_cost: '1.00',
-            eval_cost: '71.90',
-            prefix_cost: '72.90',
-            data_read_per_join: '202K',
+            query_cost: '72.90',
           },
-          used_columns: ['sku', 'iname'],
+          table: {
+            table_name: 'Item',
+            access_type: 'ALL',
+            rows_examined_per_scan: 719,
+            rows_produced_per_join: 719,
+            filtered: '100.00',
+            cost_info: {
+              read_cost: '1.00',
+              eval_cost: '71.90',
+              prefix_cost: '72.90',
+              data_read_per_join: '202K',
+            },
+            used_columns: ['sku', 'iname'],
+          },
         },
       },
-    },
-    null,
-    2
-  ),
-  extensions,
-})
+      null,
+      2
+    ),
+    extensions,
+  })
+}
+
+export const state = createState(extensions)
--- apps/mysql-workbench/src/lib/sql/visualizer.ts
+++ apps/mysql-workbench/src/lib/sql/visualizer.ts
@@ -1,7 +1,16 @@
 import mermaid from 'mermaid'
+import { popup, type PopupSettings } from '@skeletonlabs/skeleton'
 import ExplainedDataParser from './parser'
 
+const popupOptions: PopupSettings = {
+  event: 'click',
+  target: 'popupClick',
+  placement: 'top',
+}
+
 async function renderFlowchart(parser: ExplainedDataParser) {
+  const bootstrap = await import('bootstrap')
+
   let renderingText = parser.buildMermaidContent()
 
   renderingText = `graph BT;\n${renderingText}`.trim()
@@ -10,6 +19,31 @@ async function renderFlowchart(parser: ExplainedDataParser) {
     console.log(err)
     return { svg: '' }
   })
+
+  setTimeout(() => {
+    document.querySelectorAll<HTMLElement>('.node').forEach(element => {
+      const keys = element.id.split('-') ?? [];
+      const newKeys = keys.splice(1, keys.length - 2);
+      const target = newKeys.join('-');
+      const explainContent = parser.getExplainContentById(target);
+
+      if (explainContent) {
+        const content = document.createElement('div')
+        content.className = 'card p-4 variant-filled-primary'
+        content.innerHTML = explainContent
+
+        content.setAttribute('data-popup', target)
+        document.body.appendChild(content)
+
+        popup(element, { ...popupOptions, target })
+
+      }
+    })
+
+    document.querySelectorAll('[data-bs-toggle="popover"]').forEach(element => {
+      new bootstrap.Popover(element)
+    })
+  }, 1000);
 
   return rendered.svg
 }
